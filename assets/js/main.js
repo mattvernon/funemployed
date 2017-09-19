9c0e4db38d34a6b03fd9a9d8a7080cf250837732
Matthew Vernon <hello@matthewvernon.co>
2017-09-19 14:12:08 +1000

    added in animated logo on landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+// bodymovin
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bodymovin'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: false,
+  path: 'assets/js/data_A.json'
+})
+
+$('#bodymovin').mouseenter(function() {
+  animation.play()
+})
+
+$('#bodymovin').mouseleave(function() {
+  animation.stop()
+})
+
 // mobile nav
 
 $('#nav-icon3').click(function(){
